a85a9fcec4fb390a73b6672ffd0d65a11e6356f8
krushna2507 <krushna.nepate22@pccoepune.org>
2025-04-15 14:23:13 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -47,7 +47,7 @@ export default function Navigation() {
               Contact
             </Link>
             <a
-              href="https://drive.google.com/file/d/10mlgWkeHJKNDcZmR69l75VHSKT89Hv6g/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1Z43bXwd7wYK9e2tNdF9oMWt2bHPL1FZi/view?usp=sharing"
               download="krushna-nepate-resume.pdf"
               className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
             >
